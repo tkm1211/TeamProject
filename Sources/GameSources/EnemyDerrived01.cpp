75ed10789cb86177b745b7aeb8fc037c80369fc2
MSI\2210013 <NA.YU>
2021-08-24 14:11:54 +0900

    EnemyDerived01 テレサ

--- Sources/GameSources/EnemyDerrived01.cpp
+++ Sources/GameSources/EnemyDerrived01.cpp
@@ -7,6 +7,9 @@ EnemyDerrived01::EnemyDerrived01()
 {
 	model = std::make_shared<Model>("Data/Assets/Model/Enemy/enemy.fbx", false);
 	Init();
+
+	speed.x = 1;
+	A = 0.0f; B = 0.1f;
 }
 
 
@@ -14,4 +17,25 @@ void EnemyDerrived01::Update(const float elapsed_time)
 {
 	// TODO: ’¼üˆÚ“®ˆ—
 	// ƒqƒ“ƒg : pos‚ðŽg—p
+    pos.x += speed.x;
+	//pos.z--;
+	timer++;
+	
+	A += B * elapsed_time;
+	if (A >=  1.0f|| A < 0.0f)
+	{
+		B = -B;
+	}
+	color={ 1.0f, 1.0f, 1.0f, A };
+	//0.5fˆÈ‰º‚É‚È‚é‚ÆÁ‚¦‚½
+	//A‚Ì‰Šú‚ª‚O‚¾‚©‚ç‚Í‚¶‚ßÁ‚¦‚é‚Í‚¸‚È‚Ì‚Éˆêu‚¾‚¯•\Ž¦‚³‚ê‚é
+	if (timer % 70 == 0)
+	{
+		speed.x = -speed.x;
+	}
+	
+	/*if (pos.x >= 40 || pos.x < -40)
+	{
+		speed.x = -speed.x;
+	}*/
 }
